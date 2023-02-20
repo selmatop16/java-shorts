d0a3dfc126b1e4b59a612360df23f96fb32c1cf4
selmatop <selmatop16@gmail.com>
2023-02-20 11:05:31 -0500

    added Hello.java

---
+++ src/main/java/ArrayList.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayList {
+    //arrayList stores objects
+    //does not support primitives
+    //it is a class, child of list interface
+    public static void main(String[] args) {
+       ArrayList list = new ArrayList();
+    }
+}
