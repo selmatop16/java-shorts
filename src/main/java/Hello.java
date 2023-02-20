588ad5b1cbb717321298f4b034147bc3ee61fa17
selmatop <selmatop16@gmail.com>
2023-02-20 14:30:51 -0500

    added more printout

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
-//changres
+//changes
+        System.out.println("hello");
+        System.out.println("hello");
         System.out.println("hello");
     }
 }
